14f8310370b1d309e6a8dcbaba4e2585342a1ca5
THOMAS <thomas@liljegren.dk>
2015-02-18 10:04:26 +0100

    Opstart 2

---
+++ src/Data/IOperatoerDAO.java
@@ -0,0 +1,14 @@
+package Data;
+
+public interface IOperatoerDAO {
+	OperatoerDTO getOperatoer(int oprId) throws DALException;
+	List<OperatoerDTO> getOperatoerList() throws DALException;
+	void createOperatoer(OperatoerDTO opr) throws DALException;
+	void updateOperatoer(OperatoerDTO opr) throws DALException;
+	
+	public class DALException extends Exception {
+		public DALException() {
+			super("Data Access Error");
+		}
+	}
+}
