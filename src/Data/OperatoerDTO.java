951e877fbdab5cf4fff1354c0c92668cab412af3
THOMAS <thomas@liljegren.dk>
2015-02-18 10:04:12 +0100

    Opstart

---
+++ src/Data/OperatoerDTO.java
@@ -0,0 +1,42 @@
+package Data;
+
+public class OperatoerDTO {
+	
+	
+	private int oprId;                     
+	private String oprNavn;                
+	private String ini;                 
+	private String cpr;                 
+	private String password;
+	
+	public int getOprId() {
+		return oprId;
+	}
+	public void setOprId(int oprId) {
+		this.oprId = oprId;
+	}
+	public String getOprNavn() {
+		return oprNavn;
+	}
+	public void setOprNavn(String oprNavn) {
+		this.oprNavn = oprNavn;
+	}
+	public String getIni() {
+		return ini;
+	}
+	public void setIni(String ini) {
+		this.ini = ini;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCpr() {
+		return cpr;
+	}
+	
+	
+
+}
