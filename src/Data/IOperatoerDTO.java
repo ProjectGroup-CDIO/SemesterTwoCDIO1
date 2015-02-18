e2394ebfb5cd64a4b12945903e00b13513451aa2
Martin Vieth <martin_123_vieth@hotmail.com>
2015-02-18 13:26:13 +0100

    OperatorDTO done, mangler at teste den

--- src/Data/IOperatoerDTO.java
+++ src/Data/IOperatoerDTO.java
@@ -3,14 +3,14 @@ package Data;
 public interface IOperatoerDTO {
 
 	public int getOprId(String cpr) throws DataException;
-	public void setOprId(int oprId);
-	public String getOprNavn();
-	public void setOprNavn(String oprNavn);
-	public String getIni();
-	public void setIni(String ini);
-	public String getPassword();
-	public void setPassword(String password);
-	public String getCpr();
+	public void setOprId(int currentId,int newId) throws DataException;
+	public String getOprNavn(int oprId) throws DataException;
+	public void setOprNavn(String oprNavn, int oprId) throws DataException;
+	public String getIni(int oprId) throws DataException;
+	public void setIni(String ini, int oprId) throws DataException;
+	public String getPassword(int oprId) throws DataException;
+	public void setPassword(String password,int oprId) throws DataException;
+	public String getCpr(int oprId) throws DataException;
 
 	public class DataException extends Exception{
 		String ErrorMessage = "Missing Data!?!?";
--- src/Data/OperatoerDTO.java
+++ src/Data/OperatoerDTO.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 
 public class OperatoerDTO implements IOperatoerDTO{
-	
+
 	private class Operatoer{
 		int oprId;                     
 		String oprNavn;                
 		String ini;                 
 		String cpr;                 
 		String password;
-		
+
 		public Operatoer(int oprId, String oprNavn, String ini, String cpr, String password){
 			super();
 			this.oprId = oprId;
@@ -19,28 +19,28 @@ public class OperatoerDTO implements IOperatoerDTO{
 			this.ini = ini;
 			this.cpr = cpr;
 			this.password = password;
-			
+
 		}
 	}
-	
-	
+
+
 	private ArrayList<Operatoer> operatoer ; 
 
 	public OperatoerDTO(){
-		
+
 		operatoer = new ArrayList<Operatoer>();
 
 		// Tilf¿j personer
-		
+
 		operatoer.add(new Operatoer(10,"Lars Larsen","LaLa","123456-7890","123"));
 		operatoer.add(new Operatoer(11,"Helmut huthut","HeHu","012345-6789","123"));
 		operatoer.add(new Operatoer(12,"Peder Person","PePe","111111-2222","123"));
 		operatoer.add(new Operatoer(13,"Test Testason","Tete","222222-3333","123"));
-		
+
 
 	}
 
-	
+
 	public int getOprId(String cpr)throws DataException {
 		int i = 0;
 		while(true){
@@ -54,34 +54,124 @@ public class OperatoerDTO implements IOperatoerDTO{
 			i++;
 
 		}
-		
+
 	}
-	public void setOprId(int oprId) {
-		this.oprId = oprId;
-	}
-	public String getOprNavn() {
-		return oprNavn;
-	}
-	public void setOprNavn(String oprNavn) {
-		this.oprNavn = oprNavn;
-	}
-	public String getIni() {
-		return ini;
-	}
-	public void setIni(String ini) {
-		this.ini = ini;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getCpr() {
-		return cpr;
-	}
+	public void setOprId(int currentoprId, int newoprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == currentoprId){
+					operatoer.get(i).oprId = newoprId;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
 
 
+			}
+		}
+	}
+	public String getOprNavn(int oprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == oprId){
+					return operatoer.get(i).oprNavn;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
+
+
+			}
+		}
+	}
+	public void setOprNavn(String oprNavn, int oprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == oprId){
+					operatoer.get(i).oprNavn = oprNavn;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
+
+
+			}
+		}
+	}
+	public String getIni(int oprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == oprId){
+					return operatoer.get(i).ini;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
+
+
+			}
+		}
+	}
+	public void setIni(String ini, int oprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == oprId){
+					operatoer.get(i).ini = ini;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
+
+
+			}
+		}
+
+
+	}
+	public String getPassword(int oprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == oprId){
+					return operatoer.get(i).password;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
+
+
+			}
+		}
+
+	}
+	public void setPassword(String password,int oprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == oprId){
+					operatoer.get(i).password = password;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
+
+
+			}
+		}
+	}
+	public String getCpr(int oprId) throws DataException {
+		int i = 0;
+		while(true){
+			try{
+				if(operatoer.get(i).oprId == oprId){
+					return operatoer.get(i).cpr;
+				}
+			}catch(IndexOutOfBoundsException e){
+				throw new DataException("Fandt ikke et oprId der passsede");
+
+
+			}
+		}
+
+	}
 
 
 }
