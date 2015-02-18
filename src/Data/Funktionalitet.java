23b83257e905e3189fbd6c832bae2e752d9672a1
Martin Vieth <martin_123_vieth@hotmail.com>
2015-02-18 10:36:29 +0100

    Arbejder med funktionalitet for sysadmin

--- src/Data/Funktionalitet.java
+++ src/Data/Funktionalitet.java
@@ -3,5 +3,15 @@ package Data;
 public class Funktionalitet {
 	
 	IOperatoerDAO OPdata = new OperatoerDTO();
+	
+	public void createOperator(OperatoerDTO opr){
+		
+	}
+	public void deleteOperator(OperatoerDTO opr){
+	
+	}
+	public void UpdateOperator(){
+		
+	}
 
 }
