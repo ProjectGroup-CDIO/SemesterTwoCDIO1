92103df86b025a898312456f0e1305154b28dd86
Peter Asmussen <pasmussen1993@gmail.com>
2015-02-23 16:54:23 +0100

    Udkommenteret getters/setters og OperatoerArrayList i DTO, og rykket dataopretning/opbevaring til DAO

--- src/Data/OperatoerDAO.java
+++ src/Data/OperatoerDAO.java
@@ -1,10 +1,55 @@
 package Data;
 
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class OperatoerDAO implements IOperatoerDAO {
 
 	IOperatoerDTO e = new OperatoerDTO();
+	
+	
+
+	private class Operatoer{
+		int oprId;                     
+		String oprNavn;                
+		String ini;                 
+		String cpr;                 
+		String password;
+
+		public Operatoer(int oprId, String oprNavn, String ini, String cpr, String password){
+			super();
+			this.oprId = oprId;
+			this.oprNavn = oprNavn;
+			this.ini = ini;
+			this.cpr = cpr;
+			this.password = password;
+
+		}
+	}
+
+	
+	
+	private ArrayList<Operatoer> DATAList;
+ 	public OperatoerDAO(){
+
+		DATAList = new ArrayList<Operatoer>();
+
+		// ADD POEPLE
+
+		DATAList.add(new Operatoer(10,"Lars Larsen","LaLa","123456-7890","123"));
+		DATAList.add(new Operatoer(11,"Helmut huthut","HeHu","012345-6789","123"));
+		DATAList.add(new Operatoer(12,"Peder Person","PePe","111111-2222","123"));
+		DATAList.add(new Operatoer(13,"Test Testason","Tete","222222-3333","123"));
+
+
+	}
+
+	
+	
+	
+	
 	
 	@Override
 	public OperatoerDTO getOperatoer(int oprId) throws DALException {
--- src/Data/OperatoerDTO.java
+++ src/Data/OperatoerDTO.java
@@ -5,173 +5,141 @@ import java.util.ArrayList;
 
 public class OperatoerDTO implements IOperatoerDTO{
 
-	private class Operatoer{
-		int oprId;                     
-		String oprNavn;                
-		String ini;                 
-		String cpr;                 
-		String password;
-
-		public Operatoer(int oprId, String oprNavn, String ini, String cpr, String password){
-			super();
-			this.oprId = oprId;
-			this.oprNavn = oprNavn;
-			this.ini = ini;
-			this.cpr = cpr;
-			this.password = password;
-
-		}
-	}
-
-
-	private ArrayList<Operatoer> operatoer ; 
-
-	public OperatoerDTO(){
-
-		operatoer = new ArrayList<Operatoer>();
-
-		// Tilf¿j personer
-
-		operatoer.add(new Operatoer(10,"Lars Larsen","LaLa","123456-7890","123"));
-		operatoer.add(new Operatoer(11,"Helmut huthut","HeHu","012345-6789","123"));
-		operatoer.add(new Operatoer(12,"Peder Person","PePe","111111-2222","123"));
-		operatoer.add(new Operatoer(13,"Test Testason","Tete","222222-3333","123"));
-
-
-	}
-
-
-	public int getOprId(String cpr)throws DataException {
-		int i = 0;
-		while(true){
-			try {
-				if (operatoer.get(i).cpr.equals(cpr)){
-					return operatoer.get(i).oprId;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et cpr nr der passsede");
-			}
-			i++;
-
-		}
-
-	}
-	public void setOprId(int currentoprId, int newoprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == currentoprId){
-					operatoer.get(i).oprId = newoprId;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-	}
-	public String getOprNavn(int oprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == oprId){
-					return operatoer.get(i).oprNavn;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-	}
-	public void setOprNavn(String oprNavn, int oprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == oprId){
-					operatoer.get(i).oprNavn = oprNavn;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-	}
-	public String getIni(int oprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == oprId){
-					return operatoer.get(i).ini;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-	}
-	public void setIni(String ini, int oprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == oprId){
-					operatoer.get(i).ini = ini;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-
-
-	}
-	public String getPassword(int oprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == oprId){
-					return operatoer.get(i).password;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-
-	}
-	public void setPassword(String password,int oprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == oprId){
-					operatoer.get(i).password = password;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-	}
-	public String getCpr(int oprId) throws DataException {
-		int i = 0;
-		while(true){
-			try{
-				if(operatoer.get(i).oprId == oprId){
-					return operatoer.get(i).cpr;
-				}
-			}catch(IndexOutOfBoundsException e){
-				throw new DataException("Fandt ikke et oprId der passsede");
-
-
-			}
-		}
-
-	}
-
+	int oprId;                     
+	String oprNavn;                
+	String ini;                 
+	String cpr;                 
+	String password;
 
 }
+
+
+//
+//	public int getOprId(String cpr)throws DataException {
+//		int i = 0;
+//		while(true){
+//			try {
+//				if (operatoer.get(i).cpr.equals(cpr)){
+//					return operatoerList.get(i).oprId;
+//				}
+//			}catch(IndexOutOfBoundsException e){
+//				throw new DataException("Fandt ikke et cpr nr der passsede");
+//			}
+//			i++;
+//		}
+//	}
+//	
+////	public void setOprId(int currentoprId, int newoprId) throws DataException {
+////		int i = 0;
+////		while(true){
+////			try{
+////				if(operatoerList.get(i).oprId == currentoprId){
+////					operatoerList.get(i).oprId = newoprId;
+////				}
+////			}catch(IndexOutOfBoundsException e){
+////				throw new DataException("Fandt ikke et oprId der passsede");
+////
+////
+////			}
+////		}
+////	}
+//	
+//	public String getOprNavn(int oprId) throws DataException {
+//		int i = 0;
+//		while(true){
+//			try{
+//				if(operatoerList.get(i).oprId == oprId){
+//					return operatoerList.get(i).oprNavn;
+//				}
+//			}catch(IndexOutOfBoundsException e){
+//				throw new DataException("Fandt ikke et oprId der passsede");
+//			}
+//		}
+//	}
+//	
+////	public void setOprNavn(String oprNavn, int oprId) throws DataException {
+////		int i = 0;
+////		while(true){
+////			try{
+////				if(operatoerList.get(i).oprId == oprId){
+////					operatoerList.get(i).oprNavn = oprNavn;
+////				}
+////			}catch(IndexOutOfBoundsException e){
+////				throw new DataException("Fandt ikke et oprId der passsede");
+////
+////
+////			}
+////		}
+////	}
+//	
+//	public String getIni(int oprId) throws DataException {
+//		int i = 0;
+//		while(true){
+//			try{
+//				if(operatoerList.get(i).oprId == oprId){
+//					return operatoerList.get(i).ini;
+//				}
+//			}catch(IndexOutOfBoundsException e){
+//				throw new DataException("Fandt ikke et oprId der passsede");
+//			}
+//		}
+//	}
+//	
+////	public void setIni(String ini, int oprId) throws DataException {
+////		int i = 0;
+////		while(true){
+////			try{
+////				if(operatoerList.get(i).oprId == oprId){
+////					operatoerList.get(i).ini = ini;
+////				}
+////			}catch(IndexOutOfBoundsException e){
+////				throw new DataException("Fandt ikke et oprId der passsede");
+////
+////
+////			}
+////		}
+////
+////
+////	}
+//	
+//	public String getPassword(int oprId) throws DataException {
+//		int i = 0;
+//		while(true){
+//			try{
+//				if(operatoerList.get(i).oprId == oprId){
+//					return operatoerList.get(i).password;
+//				}
+//			}catch(IndexOutOfBoundsException e){
+//				throw new DataException("Fandt ikke et oprId der passsede");
+//			}
+//		}
+//	}
+//	
+////	public void setPassword(String password,int oprId) throws DataException {
+////		int i = 0;
+////		while(true){
+////			try{
+////				if(operatoerList.get(i).oprId == oprId){
+////					operatoerList.get(i).password = password;
+////				}
+////			}catch(IndexOutOfBoundsException e){
+////				throw new DataException("Fandt ikke et oprId der passsede");
+////
+////
+////			}
+////		}
+////	}
+//	
+//	public String getCpr(int oprId) throws DataException {
+//		int i = 0;
+//		while(true){
+//			try{
+//				if(operatoerList.get(i).oprId == oprId){
+//					return operatoerList.get(i).cpr;
+//				}
+//			}catch(IndexOutOfBoundsException e){
+//				throw new DataException("Fandt ikke et oprId der passsede");
+//			}
+//		}
+//	}
+//}
