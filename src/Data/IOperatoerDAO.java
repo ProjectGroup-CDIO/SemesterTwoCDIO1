ddd7d73f47060ede9750e533ae6b123ef430f69b
LibanDirir <LibanDirir@10.16.253.119>
2015-02-26 13:38:02 +0100

    fixed CreateOpertor

--- src/Data/IOperatoerDAO.java
+++ src/Data/IOperatoerDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IOperatoerDAO {
 	OperatoerDTO getOperatoer(int oprId) throws DALException;
 	List<OperatoerDTO> getOperatoerList() throws DALException;
-	void createOperatoer(OperatoerDTO opr) throws DALException;
+	void createOperatoer(int oprId) throws DALException;
 	void updateOperatoer(OperatoerDTO opr) throws DALException;
 	void deleteOperatoer(OperatoerDTO opr)throws DALException;
 	
