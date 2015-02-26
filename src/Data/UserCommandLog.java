6b91e7315766d535ab91198c4193d3c3386743a3
Kenneth Frandsen <Frandsen86@gmail.com>
2015-02-26 12:45:38 +0100

    Oprettelse af logfilen så der kan videre skrives forhver gang der laves en ændring (log af/på, commando og tid).

--- src/Data/UserCommandLog.java
+++ src/Data/UserCommandLog.java
@@ -1,26 +1,27 @@
 package Data;
 
 import java.io.BufferedWriter;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class UserCommandLog {
 	public static void main (String []args)
 	{
-		Writer writer = null;
 
-		try {
-		    writer = new BufferedWriter(new OutputStreamWriter(
-		          new FileOutputStream("LogFil.txt"), "utf-8"));
-		    writer.write("Something " + "mere" + "meget mere" + '\n');
-		    
-		} catch (IOException ex) {
-		  // report
-		} finally {
-		   try {writer.close();} catch (Exception ex) {}
+		Calendar cal = Calendar.getInstance();
+    	cal.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		    try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("LogFil.txt", true)))) {
+			    out.println("User "+ "Commando " + sdf.format(cal.getTime()));
+			}catch (IOException e) {
+			    //exception handling left as an exercise for the reader
+			
+			}
+	
 		}
+		
 	}
-}
