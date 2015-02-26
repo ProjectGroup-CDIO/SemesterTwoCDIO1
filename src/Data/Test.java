dba1489e4452bd6a52329e49e27052c5b8a50ba1
THOMAS <thomas@liljegren.dk>
2015-02-26 13:07:47 +0100

    PasswordGenerator har nu static generate()

--- src/Data/Test.java
+++ src/Data/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IFunktionalitet a = new Funktionalitet();
-		
+
 		try {
 			System.out.println(a.getOperatoer(12).ini);
 		} catch (DALException e) {
@@ -23,7 +23,6 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 
 }
