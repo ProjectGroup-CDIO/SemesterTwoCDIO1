9f140e963cff24d21ea581984fcfcdd911c6b6af
Martin Vieth <martin_123_vieth@hotmail.com>
2015-03-01 15:09:27 +0100

    formating

--- src/Data/IOperatoerDAO.java
+++ src/Data/IOperatoerDAO.java
@@ -9,6 +9,7 @@ public interface IOperatoerDAO {
 	void updateOperatoer(OperatoerDTO opr) throws DALException;
 	void deleteOperatoer(OperatoerDTO opr)throws DALException;
 	
+
 	public class DALException extends Exception {
 		public DALException() {
 			super("Data Access Error");
